67815a002f61e386accd227f2b9f6bbbe4a9b727
Ibrohim Uraimov <uraimovibrohim5@gmail.com>
2022-03-23 12:23:52 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navigate from './components/nav1/navigate';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navigate/>
+      <Routes>
+        {/* <Route path='/' element={}/> */}
+        {/* <Route path='/' element={}/> */}
+        {/* <Route path='/' element={}/> */}
+      </Routes>
+      {/* Footer */}
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/nav1/navigate.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navigate() {
+  return (
+  <>
+  <h1>Ibrohim</h1>
+  
+  
+  </>
+  )
+}
+
+export default Navigate
