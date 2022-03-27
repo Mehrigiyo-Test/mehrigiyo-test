dee9ab0f35f48cb7ec795e2da92720911439527a
Murod-de <murad101693@gmail.com>
2022-03-27 12:21:48 +0500

    from Murod

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 
 import './App.css';
 import Navigate from './components/nav1/navigate';
-<<<<<<< HEAD
 import Footer from './components/footer/footer';
 import OrganicPlantation from './components/SectionPlantation/OrganicPlantation';
 import OnlineDoctor from './components/OnlineDoctor/OnlineDoctor';
-=======
 import Footer from './components/footer/footer'
 import Home from './pages/Home/Home' 
->>>>>>> 8d115990738734a143d525965729a7c729a00134
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 function App() {
   return (
