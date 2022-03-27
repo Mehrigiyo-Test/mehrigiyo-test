4b5226cbdf7a1ac157f012e007932330b1e2b986
Alisher Umarov <umarovalisher132@gmail.com>
2022-03-27 23:58:57 +0500

    added new component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
 import Navigate from './components/nav1/navigate';
-import Footer from './components/footer/footer'
-import Home from './pages/Home/Home' 
+import Footer from './components/footer/footer';
+import Home from './pages/Home/Home'; 
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Navigate2 from './components/nav2/navigate2';
+
 function App() {
   return (
     <>
     <Router>
       <Navigate/>
+      <Navigate2/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         {/* <Route path='/' element={}/> */}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import Bannerinput from '../../components/bannerInput/bannerInput';
+import Banner from '../Banner/Banner';
+import Infosection from '../InfoSection/InfoSection';
+import Bannercard from './../../components/bannerCard/bannerCard';
 
 function Home() {
   return (
     <>
-    <>home</>
+      <Banner/>
+      <Bannerinput/>
+      <Bannercard/>
+      <Infosection/>
+      {/* <Infosection title="Tez to'lov" text="100% xavfsiz to'lov"/> */}
     </>
   )
 }
 
-export default Home
+export default Home;
