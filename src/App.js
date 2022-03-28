83b0d1435017d17c1e197b234525bc76f10d0507
KhayrullayevJafar <profrontenddev954@gmail.com>
2022-03-28 14:19:45 +0500

    question

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigate from './components/nav1/navigate';
 import Footer from './components/footer/footer'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Asked from './components/questions/asked/asked';
+import Send from './components/questions/send/send';
 function App() {
   return (
     <>
@@ -16,6 +17,7 @@ function App() {
         {/* <Route path='/' element={}/> */}
       </Routes>
       <Asked />
+      <Send />
       <Footer/>
       
     </Router>
