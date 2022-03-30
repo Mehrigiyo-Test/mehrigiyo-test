2f0b9fe269bc3f7ac2a02589805df1d0481b7226
Abdul-Aziz <lazizxonakramov@gmail.com>
2022-03-30 12:16:59 +0500

    fixed bug

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Asked from './components/questions/asked/asked';
 import Send from './components/questions/send/send';
 import Navigate2 from './components/nav2/navigate2';
-import GlobalCallSms from './components/GlobalCallSms/GlobalCallSms';
+import GlobalCallSms from './components/Global/GlobalCallSms';
 
 function App() {
   return (
