773109eeb7ab002e2e5ebc9e0a04a2b128909337
Ibrohim Uraimov <uraimovibrohim5@gmail.com>
2022-04-01 16:57:35 +0500

    for Abdulaziz slider

--- src/App.js
+++ src/App.js
@@ -2,17 +2,6 @@
 import './App.css';
 import Navigate from './components/nav1/navigate';
 import Footer from './components/footer/footer';
-<<<<<<< HEAD
-import Home from './pages/Home/Home' 
-import OnlineDoctor from './pages/OnlineDoctor/OnlineDoctor'
-import Products from './pages/Products/Products'
-import AboutUs from './pages/AboutUs/AboutUs'
-import News from './pages/News/News'
-import Help from './pages/Help/Help'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Asked from './components/questions/asked/asked';
-import Send from './components/questions/send/send';
-=======
 import Home from './pages/Home/Home';
 import OnlineDoctor from './pages/OnlineDoctor/OnlineDoctor';
 import Products from './pages/Products/Products';
@@ -25,18 +14,14 @@ import Send from './components/questions/send/send';
 import Navigate2 from './components/nav2/navigate2';
 import GlobalCallSms from './components/Global/GlobalCallSms';
 
->>>>>>> 0737f3522ecd9f973f374f664f9f4a06710882d5
 function App() {
   return (
     <>
     <Router>
       <Navigate/>
-<<<<<<< HEAD
       {/* <Navigate2/> */}
-=======
       <Navigate2/>
       <GlobalCallSms/>
->>>>>>> 0737f3522ecd9f973f374f664f9f4a06710882d5
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/onlineDoctor' element={<OnlineDoctor/>}/>
