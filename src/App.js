f1616a25db971ef37ee1322809faadab2b788bc1
Abdul-Aziz <lazizxonakramov@gmail.com>
2022-04-06 13:12:41 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import AboutUs from './pages/AboutUs/AboutUs';
 import News from './pages/News/News';
 import Help from './pages/Help/Help';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Asked from './components/questions/asked/asked';
-import Send from './components/questions/send/send';
 import Navigate2 from './components/nav2/navigate2';
 import GlobalCallSms from './components/Global/GlobalCallSms';
 
