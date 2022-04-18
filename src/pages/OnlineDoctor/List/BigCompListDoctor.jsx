f59d8eeb33bf35efda11167dff9ee7459aaefca4
Uraimoff <uraimovibrohim5@gmail.com>
2022-04-18 12:26:54 +0500

    from master

---
+++ src/pages/OnlineDoctor/List/BigCompListDoctor.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+BigCompListDoctor.propTypes = {
+    
+};
+
+function BigCompListDoctor(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default BigCompListDoctor;
