add6b49a245df95e2fe1529413ccbb9d1c9cb6e1
Uraimoff <uraimovibrohim5@gmail.com>
2022-04-25 18:34:50 +0500

    modal finished50%

--- src/components/DoctorFullInfoCard/DoctorFullCard.jsx
+++ src/components/DoctorFullInfoCard/DoctorFullCard.jsx
@@ -10,50 +10,56 @@ import TaemOfLeaf from "./../../images/TeamOfLeaf.png"
 import kidney from "./../../images/kidney5.png"
 import info from "./../../images/Vector1.png"
 
-function DoctorFullCard() {
+function DoctorFullCard({setOpen, data , props, btn, active}) {
+    const gg = ()=>
+    setOpen('')
   return (
     <>
     <div className='DocFuulINWR'>
+    {data.filter(b=> b.id === props).map(a=>(
         <div className='centerInfo'>
-            <div className='BigCircleIMg'><img src="" alt="" /></div>
+            <div className='BigCircleIMg'><img src={a.img} alt="" /></div>
             <div>
-                <div className='DoctorName'>
-                <h2>Gavhar Sobirova</h2>
-                <div className='display gap'>
-                <div className='selected'><img src={selected} alt="" /></div>
-                <div className='selected'><img src={share} alt="" /></div>
-                </div>
-                </div>
-                <div><img src={stars} alt="" /></div>
-                <div className='DocInfCards'>
-                <div className='DoctorPract'>
-                    <div>
-                    <div>Shaharlar</div>
-                    <h2 className='twoHundred'>233+</h2>
+                    <div className='DoctorName'>
+                    <h2>{a.text1}</h2>
+                    <div className='display gap'>
+                    <div className='selected'><img src={selected} alt="" /></div>
+                    <div className='selected'><img src={share} alt="" /></div>
                     </div>
-                </div>
-                <div className='DoctorPract'>
-                    <div>
-                    <div>Tajriba</div>
-                    <h2 className='five'>5 yil</h2>
                     </div>
-                </div>
-                <div className='DoctorPract'>
-                    <div>
-                        <div className='DoctorPractINfoicon'><img src={info} alt="" /></div>
-                    <div className='doctorPractImg'><img src={kidney} alt="" /></div>
-                    <p>Kardiolog</p>
+                    <div><img src={stars} alt="" /></div>
+                    <div className='DocInfCards'>
+                    <div className='DoctorPract'>
+                        <div>
+                        <div>Shaharlar</div>
+                        <h2 className='twoHundred'>233+</h2>
+                        </div>
                     </div>
-                </div>
-                </div>
-                <div className='DocPractInf'>
-                    <h6>About Doctor</h6>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra donec turpis integer ipsum, et enim orci amet.</p>
-                </div>
+                    <div className='DoctorPract'>
+                        <div>
+                        <div>Tajriba</div>
+                        <h2 className='five'>5 yil</h2>
+                        </div>
+                    </div>
+                    <div className='DoctorPract'>
+                        <div>
+                            <div className='DoctorPractINfoicon'><img src={info} alt="" /></div>
+                        <div className='doctorPractImg'><img src={kidney} alt="" /></div>
+                        <p>Kardiolog</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div className='DocPractInf'>
+                        <h6>About Doctor</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra donec turpis integer ipsum, et enim orci amet.</p>
+                    </div>
+                   
+              
             </div>
         </div>
-        <div ><button className='DocButton'>Appointment</button></div>
-        <div className='close'><img src={close} alt="" /></div>
+         ))}
+        <div ><button className='DocButton' onClick={btn}>Appointment</button></div>
+        <div className='close' onClick={gg}><img src={close} alt="" /></div>
         <div className='modalBak'><img src={barg} alt="" /></div>
         <div className='TeamLeaf'><img src={TaemOfLeaf} alt="" /></div>
     </div>
