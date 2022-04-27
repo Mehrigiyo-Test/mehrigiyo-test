281fb1c72b7b794df33f966c2b822182409ae1bf
Uraimoff <uraimovibrohim5@gmail.com>
2022-04-27 14:57:01 +0500

    Asked fixed

--- src/components/bannerPhoto/bannerPhoto.jsx
+++ src/components/bannerPhoto/bannerPhoto.jsx
@@ -4,11 +4,14 @@ import MainPhoto from '../../images/main.png';
 import './style.css';
 
 const Bannerinput = () => {
+    // function popupMsg(theMsg) {
+    //     alert(theMsg);
+    //     }
     return (
         <div className='banner-input bc-color bc-color2'>
             <div className='GlobalWrapper'>
                 <div className='main-photo'>
-                    <img src={MainPhoto} alt="photo" />
+                    <img src={MainPhoto} alt="photo" onMouseDown={()=> alert('This image is Copyrighted')}/>
                 </div>
             </div>
         </div>
--- src/components/questions/asked/asked.jsx
+++ src/components/questions/asked/asked.jsx
@@ -1,30 +1,15 @@
 import React, { useState } from "react";
 import "./asked.css";
 import { BsArrowRight } from "react-icons/bs";
-import { AiOutlinePlus, AiOutlineClose } from 'react-icons/ai';
-import leaf from '../../../images/send_background.png';
+import { AiOutlinePlus, AiOutlineClose } from "react-icons/ai";
+import leaf from "../../../images/send_background.png";
 
 export default function Asked() {
-  const [show, setShow] = useState(false);
-  const [show2, setShow2] = useState(false);
-  const [show3, setShow3] = useState(false);
-  const [show4, setShow4] = useState(false);
-  const [show5, setShow5] = useState(false);
-  const handleChange = () => {
-    setShow(!show);
-  };
-  const handleChange2 = () => {
-    setShow2(!show2);
-  };
-  const handleChange3 = () => {
-    setShow3(!show3);
-  };
-  const handleChange4 = () => {
-    setShow4(!show4);
-  };
-  const handleChange5 = () => {
-    setShow5(!show5);
-  };
+  const [show, setShow] = useState(true);
+  const [show2, setShow2] = useState(true);
+  const [show3, setShow3] = useState(true);
+  const [show4, setShow4] = useState(true);
+  const [show5, setShow5] = useState(true);
 
   return (
     <div className="asked-background">
@@ -43,128 +28,216 @@ export default function Asked() {
             </button>
           </div>
           <div className="accordion">
+            <div className="big-div">
+              <div className="section">
+                <div className="text">
+                  <b>01</b>{" "}
+                  <span className="accordion-txt">
+                    Какой состав сока Alatoo?
+                  </span>{" "}
+                </div>
 
-          <div className="big-div">
-            <div className="section" onClick={handleChange}>
-              <div className="text"><b>01</b> <span className="accordion-txt">Какой состав сока Alatoo?</span> </div>
-              {!show ? (
-                <div className="plus">
-                  <AiOutlinePlus />
-                </div>
-              ) : (
-                <div className="minus">
-                  <AiOutlineClose />
-                </div>
-              )}
-            </div>
-            <div className="section-text">
-              {show ? (
-                <div className="more-text">
-                  Детокс сок Алатоо имеет очень простой состав. Это сок зеленой папайи и сок горького арбуза. В свою очередь эти два компонента включают в себя множество полезных ферментов, микроэлементов и витаминов, таких как органические кислоты, витамин с, витамин b1, витамин d, а также фермент папаин.
-                </div>
-              ) : null}
-            </div>
-
-            <div className="section2" onClick={handleChange2}>
-              <div className="text2"><b>02</b> <span className="accordion-txt"> Сколько стоит Алатоо и сколько стоит доставка?</span></div>
-              {!show2 ? (
-                <div className="plus2">
-                  <AiOutlinePlus />
-                </div>
-              ) : (
-                <div className="minus2">
-                  <AiOutlineClose />
-                </div>
-              )}
-            </div>
-            <div className="section-text2">
-              {show2 ? (
-                <div className="more-text2">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Minima aut dicta nam libero voluptatem porro non itaque culpa,
-                  laboriosam, nemo, nisi similique iusto suscipit modi animi in
-                  natus beatae. Maiores?
-                </div>
-              ) : null}
-            </div>
-
-            <div className="section3" onClick={handleChange3}>
-              <div className="text3"><b>03</b> <span className="accordion-txt"> Здравствуйте, аллергия часто на этот сок?</span></div>
-              {!show3 ? (
-                <div className="plus3">
-                  <AiOutlinePlus />
-                </div>
-              ) : (
-                <div className="minus3">
-                  <AiOutlineClose />
-                </div>
-              )}
-            </div>
-            <div className="section-text3">
-              {show3 ? (
-                <div className="more-text3">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Minima aut dicta nam libero voluptatem porro non itaque culpa,
-                  laboriosam, nemo, nisi similique iusto suscipit modi animi in
-                  natus beatae. Maiores?
-                </div>
-              ) : null}
-            </div>
-
-            <div className="section4" onClick={handleChange4}>
-              <div className="text4">
-                  <b>04</b> <span className="accordion-txt"> Эффективен ли сок Алатоо при часто рецидивирующем герпесе?</span>
+                {show ? (
+                  <div className="plus" onClick={() => setShow(false)}>
+                    <AiOutlinePlus />
+                  </div>
+                ) : (
+                  <div className="minus" onClick={() => setShow(true)}>
+                    <AiOutlineClose />
+                  </div>
+                )}
               </div>
-              {!show4 ? (
-                <div className="plus4">
-                  <AiOutlinePlus />
-                </div>
-              ) : (
-                <div className="minus4">
-                  <AiOutlineClose />
-                </div>
-              )}
-            </div>
-            <div className="section-text4">
-              {show4 ? (
-                <div className="more-text4">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Minima aut dicta nam libero voluptatem porro non itaque culpa,
-                  laboriosam, nemo, nisi similique iusto suscipit modi animi in
-                  natus beatae. Maiores?
-                </div>
-              ) : null}
-            </div>
 
-            <div className="section5" onClick={handleChange5}>
-              <div className="text5"><b>05</b>  <span className="accordion-txt">Здравствуйте, можно ли принимать при полипе в жёлчном пузыре?</span></div>
-              {!show5 ? (
-                <div className="plus5">
-                  <AiOutlinePlus />
+              <div className="section-text">
+                <div
+                  className="more-text"
+                  style={
+                    show
+                      ? { height: "0", fontSize: "0px", transition: "1s" }
+                      : {
+                          height: "500",
+                          fontSize: "16px",
+                          transition: "1s",
+                          paddingBottom: "100px",
+                        }
+                  }
+                >
+                  Детокс сок Алатоо имеет очень простой состав. Это сок зеленой
+                  папайи и сок горького арбуза. В свою очередь эти два
+                  компонента включают в себя множество полезных ферментов,
+                  микроэлементов и витаминов, таких как органические кислоты,
+                  витамин с, витамин b1, витамин d, а также фермент папаин.
                 </div>
-              ) : (
-                <div className="minus5">
-                  <AiOutlineClose />
+              </div>
+              <div className="section">
+                <div className="text">
+                  <b>02</b>{" "}
+                  <span className="accordion-txt">
+                  Сколько стоит Алатоо и сколько стоит доставка?
+                  </span>{" "}
                 </div>
-              )}
+
+                {show2 ? (
+                  <div className="plus" onClick={() => setShow2(false)}>
+                    <AiOutlinePlus />
+                  </div>
+                ) : (
+                  <div className="minus" onClick={() => setShow2(true)}>
+                    <AiOutlineClose />
+                  </div>
+                )}
+              </div>
+
+              <div className="section-text">
+                <div
+                  className="more-text"
+                  style={
+                    show2
+                      ? { height: "0", fontSize: "0px", transition: "1s" }
+                      : {
+                          height: "500",
+                          fontSize: "16px",
+                          transition: "1s",
+                          paddingBottom: "100px",
+                        }
+                  }
+                >
+                  Детокс сок Алатоо имеет очень простой состав. Это сок зеленой
+                  папайи и сок горького арбуза. В свою очередь эти два
+                  компонента включают в себя множество полезных ферментов,
+                  микроэлементов и витаминов, таких как органические кислоты,
+                  витамин с, витамин b1, витамин d, а также фермент папаин.
+                </div>
+              </div>
+
+              <div className="section">
+                <div className="text">
+                  <b>03</b>{" "}
+                  <span className="accordion-txt">
+                  Здравствуйте, аллергия часто на этот сок?
+                  </span>{" "}
+                </div>
+
+                {show3 ? (
+                  <div className="plus" onClick={() => setShow3(false)}>
+                    <AiOutlinePlus />
+                  </div>
+                ) : (
+                  <div className="minus" onClick={() => setShow3(true)}>
+                    <AiOutlineClose />
+                  </div>
+                )}
+              </div>
+
+              <div className="section-text">
+                <div
+                  className="more-text"
+                  style={
+                    show3
+                      ? { height: "0", fontSize: "0px", transition: "1s" }
+                      : {
+                          height: "500",
+                          fontSize: "16px",
+                          transition: "1s",
+                          paddingBottom: "100px",
+                        }
+                  }
+                >
+                  Детокс сок Алатоо имеет очень простой состав. Это сок зеленой
+                  папайи и сок горького арбуза. В свою очередь эти два
+                  компонента включают в себя множество полезных ферментов,
+                  микроэлементов и витаминов, таких как органические кислоты,
+                  витамин с, витамин b1, витамин d, а также фермент папаин.
+                </div>
+              </div>
+              <div className="section">
+                <div className="text">
+                  <b>04</b>{" "}
+                  <span className="accordion-txt">
+                  Эффективен ли сок Алатоо при часто рецидивирующем герпесе?
+                  </span>{" "}
+                </div>
+
+                {show4 ? (
+                  <div className="plus" onClick={() => setShow4(false)}>
+                    <AiOutlinePlus />
+                  </div>
+                ) : (
+                  <div className="minus" onClick={() => setShow4(true)}>
+                    <AiOutlineClose />
+                  </div>
+                )}
+              </div>
+
+              <div className="section-text">
+                <div
+                  className="more-text"
+                  style={
+                    show4
+                      ? { height: "0", fontSize: "0px", transition: "1s" }
+                      : {
+                          height: "500",
+                          fontSize: "16px",
+                          transition: "1s",
+                          paddingBottom: "100px",
+                        }
+                  }
+                >
+                  Детокс сок Алатоо имеет очень простой состав. Это сок зеленой
+                  папайи и сок горького арбуза. В свою очередь эти два
+                  компонента включают в себя множество полезных ферментов,
+                  микроэлементов и витаминов, таких как органические кислоты,
+                  витамин с, витамин b1, витамин d, а также фермент папаин.
+                </div>
+              </div>
+              <div className="section">
+                <div className="text">
+                  <b>05</b>{" "}
+                  <span className="accordion-txt">
+                  Здравствуйте, можно ли принимать при полипе в жёлчном пузыре?
+                  </span>{" "}
+                </div>
+
+                {show5 ? (
+                  <div className="plus" onClick={() => setShow5(false)}>
+                    <AiOutlinePlus />
+                  </div>
+                ) : (
+                  <div className="minus" onClick={() => setShow5(true)}>
+                    <AiOutlineClose />
+                  </div>
+                )}
+              </div>
+
+              <div className="section-text">
+                <div
+                  className="more-text"
+                  style={
+                    show5
+                      ? { height: "0", fontSize: "0px", transition: "1s" }
+                      : {
+                          height: "500",
+                          fontSize: "16px",
+                          transition: "1s",
+                          paddingBottom: "100px",
+                        }
+                  }
+                >
+                  Детокс сок Алатоо имеет очень простой состав. Это сок зеленой
+                  папайи и сок горького арбуза. В свою очередь эти два
+                  компонента включают в себя множество полезных ферментов,
+                  микроэлементов и витаминов, таких как органические кислоты,
+                  витамин с, витамин b1, витамин d, а также фермент папаин.
+                </div>
+              </div>
+
             </div>
-            <div className="section-text5">
-              {show5 ? (
-                <div className="more-text5">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Minima aut dicta nam libero voluptatem porro non itaque culpa,
-                  laboriosam, nemo, nisi similique iusto suscipit modi animi in
-                  natus beatae. Maiores?
-                </div>
-              ) : null}
-            </div>
-          </div>
           </div>
         </div>
       </div>
-            <div className="leaf">
-              <img src={leaf} alt="" />
-            </div>
+      <div className="leaf">
+        <img src={leaf} alt="" />
+      </div>
     </div>
   );
 }
