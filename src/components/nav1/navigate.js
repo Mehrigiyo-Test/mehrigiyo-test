a3c46edd989b8707056466b3c5a83a72a61b110e
Murod-de <murad101693@gmail.com>
2022-05-25 16:40:28 +0500

    adasd

--- src/components/nav1/navigate.js
+++ src/components/nav1/navigate.js
@@ -1,61 +1,66 @@
-import React from 'react'
-import "./navStyles.css"
-import lokatsa from '../../images/Vector.png'
-import profile from '../../images/profile.png'
-import Dropdown from '../Dropdown/Dropdown'
+import React from "react";
+import "./navStyles.css";
+import lokatsa from "../../images/Vector.png";
+import profile from "../../images/profile.png";
+import Dropdown from "../Dropdown/Dropdown";
+import Api from "../../Servis/ServisApi";
+
+
 
 function Navigate() {
   const nav = [
     {
-      name: "Mahsulotlar katalogi"
+      name: "Mahsulotlar katalogi",
     },
     {
-      name: "Foto-lavhalar"
+      name: "Foto-lavhalar",
     },
     {
-      name: "Yetkazib berish va to'lash"
+      name: "Yetkazib berish va to'lash",
     },
     {
-      name: "Aksiya"
+      name: "Aksiya",
     },
     {
-      name: "Yangi"
-    }
-  ]
+      name: "Yangi",
+    },
+  ];
+
+  Api.get('/specialist/doctors/')
+    .then((response) => console.log(response))
 
   return (
     <div className="navigate">
-      <div className='navigate__wrapper GlobalWrapper'>
-        <nav className='nav1'>
-          <ul className='ul'>
-            {nav.map(item => (
+      <div className="navigate__wrapper GlobalWrapper">
+        <nav className="nav1">
+          <ul className="ul">
+            {nav.map((item) => (
               <li>{item.name}</li>
             ))}
           </ul>
         </nav>
 
-        <div className='navigate2'>
-         <Dropdown/>
-          <div className='location'>
-            <p className='locat'>
-              Toshkent shahar
-            </p>
-            <div className='lokatsa'>
+        <div className="navigate2">
+          <Dropdown />
+          <div className="location">
+            <p className="locat">Toshkent shahar</p>
+            <div className="lokatsa">
               <img src={lokatsa} />
             </div>
           </div>
-          <div className='login'>
-            <p className='locat'>
-              Kirish
-            </p>
-            <div className='profil'> <img src={profile} /></div>
-          </div>
-
+          <a href="http://localhost:3001/">
+            <div className="login">
+              <p className="locat">Kirish</p>
+              <div className="profil">
+                {" "}
+                <img src={profile} />
+              </div>
+            </div>
+          </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navigate
-
+export default Navigate;
