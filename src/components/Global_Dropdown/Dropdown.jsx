01e7ac4b41413f467700d07dcaa8bed311b87945
Murod-de <murad101693@gmail.com>
2022-05-27 18:14:17 +0500

    fixed

--- src/components/Global_Dropdown/Dropdown.jsx
+++ src/components/Global_Dropdown/Dropdown.jsx
@@ -4,24 +4,28 @@ import { useState } from "react";
 
 export default function Dropdown({ title, link, items }) {
   const [open, setOpen] = useState(false);
-
+  
   return (
     <div className="dropdown">
-      <div className="nav_Link" onClick={() => setOpen(!open)}>
-        <NavLink to={link ? true : false}>{title}</NavLink>
+      <div className="nav_Link" onClick={()=>setOpen(!open)}>
+        <NavLink className="nav_Item" to={link ? true : false}>
+          {title}
+        </NavLink>
       </div>
 
       {open ? (
         <div className="dropdownBox">
-          <section className="section_Container" onClick={() => setOpen(false)}>
+          <section className="section_Container" onClick={() => setOpen(!open)}>
             {items.map((item, index) => (
-              <div
-                key={index}
-                className="dropdown_Item"
-                onClick={() => setOpen(!open)}
-              >
-                <Link to={link}>{item.title}</Link>
-              </div>
+              <Link to={link}>
+                <div
+                  key={index}
+                  className="dropdown_Item"
+                  onClick={() => setOpen(!open)}
+                >
+                  <Link to={link}>{item.title}</Link>
+                </div>
+              </Link>
             ))}
           </section>
         </div>
--- src/components/nav2/option/option.jsx
+++ src/components/nav2/option/option.jsx
@@ -8,7 +8,7 @@ function Option(link) {
     <div>
         <div><img src={Polygon} alt="" /></div>
         <select className='optionWrapper'>
-            <option className='link55'>{link}Nevropatolog</option>
+            <option className='link55'>{link}</option>
         </select>
     </div>
     </>
