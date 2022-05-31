717744771e6364fd2b2b3576722518ab362d0da8
Abdulaziz <lazizxonakramov@gmail.com>
2022-05-31 17:35:55 +0500

    asd

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,91 +1,114 @@
-import React from 'react';
+import React from "react";
 import Groupffff from "../../images/Groupffff.png";
-import './NewStyle.scss'
-import LastNews from '../../components/NewPageComp/lastNews/lastNews';
-import RowNewsCard from '../../components/NewPageComp/RowNewsCard/RowNewsCard';
-import Group from "./../../images/Group.png"
-import rowImg from "./../../images/rowImg.png"
-import Placeholder from "./../../images/Placeholder.png"
-import ColumnCardTxt from './../../components/NewPageComp/ColumnCardNewsTxt/ColumnCardTxt'
-import NewsCard from '../../components/NewsCard/NewsCard';
-import sliderEx from "./../../images/sliderEx.png"
-import { useSelector } from 'react-redux';
+import "./NewStyle.scss";
+import LastNews from "../../components/NewPageComp/lastNews/lastNews";
+import RowNewsCard from "../../components/NewPageComp/RowNewsCard/RowNewsCard";
+import Group from "./../../images/Group.png";
+import rowImg from "./../../images/rowImg.png";
+import Placeholder from "./../../images/Placeholder.png";
+import ColumnCardTxt from "./../../components/NewPageComp/ColumnCardNewsTxt/ColumnCardTxt";
+import NewsCard from "../../components/NewsCard/NewsCard";
+import sliderEx from "./../../images/sliderEx.png";
+import { useSelector } from "react-redux";
 
 function News() {
-  const { data } = useSelector((state) => state.getDoctors);
-  console.log(data, "BU DATA2");
+  const { data } = useSelector((state) => state.getNews);
+
+  const ad = data.map((_, index) => index);
+  const er = ad[ad.length - 1];
+
+  const rev = data
+    .filter((_, i) => i < er)
+    .map((item) => item)
+    .reverse();
+  console.log(rev, "BU DATA2");
+
+  const API = "http://207.154.244.140:8000/";
+
   return (
     <>
-    <div className='News'>
-    <div className='News_wrapper'>
-       <div ><img className='leafs' src={Groupffff} alt="" /></div>
-      <div className='plant'><img className='plantImg' src={Group} alt="" /></div>
-    <div className='LastNews_p'><LastNews/></div>
-    <div className='display'><RowNewsCard
-    img={rowImg}
-    Greenholder={Placeholder}
-    events="TADBIRLAR"
-    time="04 авг 2021 07:55"
-    theme="BARCHA KASALLIKLAR UCHUN DORI, MEHRIGIYO KOMPANIYASIDAN ..."
-    txt="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
-      />
-      <RowNewsCard
-    img={rowImg}
-    Greenholder={Placeholder}
-    events="TADBIRLAR"
-    time="04 авг 2021 07:55"
-    theme="BARCHA KASALLIKLAR UCHUN DORI, MEHRIGIYO KOMPANIYASIDAN ..."
-    txt="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
-      />
+      <div className="News">
+        <div className="News_wrapper">
+          <div>
+            <img className="leafs" src={Groupffff} alt="" />
+          </div>
+          <div className="plant">
+            <img className="plantImg" src={Group} alt="" />
+          </div>
+          <div className="LastNews_p">
+            <LastNews />
+          </div>
+          <div className="display">
+            {rev.map((item) => (
+              <RowNewsCard
+                img={API + item.image}
+                Greenholder={Placeholder}
+                events={item.hashtag}
+                time={item.create_at}
+                theme={item.name}
+                txt={item.description}
+              />
+            ))}
+          </div>
+
+          <div>
+            <ColumnCardTxt />
+          </div>
+          <div className="display NewsCardpadding">
+            <div>
+              <NewsCard
+                img={sliderEx}
+                category="#Ziravorlar"
+                time="4 min oldin"
+                theme='Mehrigiyo kompaniyasi "Detaksioma - 100% tabiy vosita" mahsulotini taqdim etadi.'
+                info="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
+              />
+              <NewsCard
+                img={sliderEx}
+                category="#Ziravorlar"
+                time="4 min oldin"
+                theme='Mehrigiyo kompaniyasi "Detaksioma - 100% tabiy vosita" mahsulotini taqdim etadi.'
+                info="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
+              />
+            </div>
+            <div>
+              <NewsCard
+                img={sliderEx}
+                category="#Ziravorlar"
+                time="4 min oldin"
+                theme='Mehrigiyo kompaniyasi "Detaksioma - 100% tabiy vosita" mahsulotini taqdim etadi.'
+                info="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
+              />
+              <NewsCard
+                img={sliderEx}
+                category="#Ziravorlar"
+                time="4 min oldin"
+                theme='Mehrigiyo kompaniyasi "Detaksioma - 100% tabiy vosita" mahsulotini taqdim etadi.'
+                info="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
+              />
+            </div>
+            <div>
+              <NewsCard
+                img={sliderEx}
+                category="#Ziravorlar"
+                time="4 min oldin"
+                theme='Mehrigiyo kompaniyasi "Detaksioma - 100% tabiy vosita" mahsulotini taqdim etadi.'
+                info="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
+              />
+              <NewsCard
+                img={sliderEx}
+                category="#Ziravorlar"
+                time="4 min oldin"
+                theme='Mehrigiyo kompaniyasi "Detaksioma - 100% tabiy vosita" mahsulotini taqdim etadi.'
+                info="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
+              />
+            </div>
+          </div>
+        </div>
       </div>
-     <div><ColumnCardTxt/></div>
-      <div className='display NewsCardpadding'><div><NewsCard
-      img={sliderEx}
-      category="#Ziravorlar"
-      time="4 min oldin"
-      theme='Mehrigiyo kompaniyasi "Detaksioma - 100% tabiy vosita" mahsulotini taqdim etadi.'
-      info="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
-      />
-      <NewsCard
-      img={sliderEx}
-      category="#Ziravorlar"
-      time="4 min oldin"
-      theme='Mehrigiyo kompaniyasi "Detaksioma - 100% tabiy vosita" mahsulotini taqdim etadi.'
-      info="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
-      /></div>
-      <div><NewsCard
-      img={sliderEx}
-      category="#Ziravorlar"
-      time="4 min oldin"
-      theme='Mehrigiyo kompaniyasi "Detaksioma - 100% tabiy vosita" mahsulotini taqdim etadi.'
-      info="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
-      />
-      <NewsCard
-      img={sliderEx}
-      category="#Ziravorlar"
-      time="4 min oldin"
-      theme='Mehrigiyo kompaniyasi "Detaksioma - 100% tabiy vosita" mahsulotini taqdim etadi.'
-      info="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
-      /></div>
-       <div><NewsCard
-      img={sliderEx}
-      category="#Ziravorlar"
-      time="4 min oldin"
-      theme='Mehrigiyo kompaniyasi "Detaksioma - 100% tabiy vosita" mahsulotini taqdim etadi.'
-      info="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
-      />
-      <NewsCard
-      img={sliderEx}
-      category="#Ziravorlar"
-      time="4 min oldin"
-      theme='Mehrigiyo kompaniyasi "Detaksioma - 100% tabiy vosita" mahsulotini taqdim etadi.'
-      info="Uning tarkibida kimyoviy o'g'it ishlatmasdan O'zbekistonning ekologik toza hududida yetishtiril-gan tabiiy komponentlar mavjud."
-      /></div></div>
-    </div>
-    </div>
     </>
-  )
-// }
+  );
+  // }
 }
 
-export default News
+export default News;
