85c1a1fa7f5e24d5b1193bbceb02fe288f276d46
Abdulaziz <lazizxonakramov@gmail.com>
2022-05-31 13:17:29 +0500

    fixed bugs

--- src/components/TopDoctors/TopDoctors.jsx
+++ src/components/TopDoctors/TopDoctors.jsx
@@ -3,7 +3,7 @@ import "./TopDoctors.scss";
 import { MdOutlineBookmarkAdd } from "react-icons/md";
 import { MdOutlineBookmarkAdded } from "react-icons/md";
 
-const TopDoctors = ({ text1,  img, setopen, func, id, setactive,}) => {
+const TopDoctors = ({ text1, img, setopen, func, id, setactive }) => {
   const gg = () => {
     setopen("open");
     setactive("active");
@@ -12,11 +12,10 @@ const TopDoctors = ({ text1,  img, setopen, func, id, setactive,}) => {
   const [clickSave, setClickSave] = useState(true);
 
   const ggg = (item) => {
-    const firstName = item.split('')[0]
-    const lastName = item.split(' ')[1]
-    return `${firstName}. ${lastName}`
-  }
- 
+    const firstName = item.split("")[0];
+    const lastName = item.split(" ")[1];
+    return `${firstName}. ${lastName}`;
+  };
 
   return (
     <div className="topDoctors">
@@ -34,7 +33,7 @@ const TopDoctors = ({ text1,  img, setopen, func, id, setactive,}) => {
         </span>
       </div>
       <p className="topDoctors__paragraph1">{ggg(text1)}</p>
-      {/* <p className="topDoctors__paragraph2">{text2}</p> */}
+      <p className="topDoctors__paragraph2">⭐️ 4.7 (233 reviews)</p>
       <button className="topDoctors__btn" onClick={gg}>
         Appoinment
       </button>
