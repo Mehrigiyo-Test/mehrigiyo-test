e93b3ee41b0520d6b0bbb47a3eb7096cd52f452b
Xurshid <nematoff4744@gmail.com>
2022-06-03 16:31:13 +0500

    update

--- src/components/User/nav2/navigate2.jsx
+++ src/components/User/nav2/navigate2.jsx
@@ -13,58 +13,36 @@ import { actionChangeRouter } from "../../../store/changeRouter/action";
 
 const Navigate2 = () => {
   const [link, setLink] = useState(true);
-  const navinfo = [
-    {
-      title: "Bosh sahifa",
-      link: "/",
-      set: true,
-      path: "user",
-    },
-    {
-      title: "Onlayn shifokorlar",
-      link: "/onlineDoctor",
-      dropdown: [
-        { title: "Nevropolog", link: "/" },
-        { title: "Genetika", link: "/" },
-        { title: "Stomatologiya", link: "/" },
-        { title: "Jarrohlik", link: "/" },
-        { title: "Hammasi +", link: "/" },
-      ],
-    },
-    {
-      title: "Mahsulotlar",
-      link: "/products",
-      dropdown: [
-        { title: "Choy", link: "/" },
-        { title: "Yog'lar", link: "/" },
-        { title: "Ziravorlar", link: "/" },
-        { title: "Asal", link: "/" },
-        { title: "Tabletkalar", link: "/" },
-        { title: "Hammasi +", link: "/" },
-      ],
-    },
-    {
-      title: "Biz haqimizda",
-      link: "/aboutUs",
-      set: false,
-      // dropdown: []
-    },
-    {
-      title: "Yangiliklar",
-      link: "/news",
-      dropdown: [
-        { title: "Yangiliklar", link: "/" },
-        { title: "Salomatlik sari", link: "/" },
-      ],
-    },
-    {
-      title: "Yordam",
-      link: "/help",
-      dropdown: [
-        { title: "Yordam", link: "/" },
-        { title: `Ko'p so'raladigan savollar`, link: "/" },
-      ],
-    },
+  const navElements = {
+    title1: `Bosh sahifa`,
+    title2: `Onlayn shifokorlar`,
+    title3: `Mahsulotlar`,
+    title4: `Biz haqimizda`,
+    title5: `Yangiliklar`,
+    title6: `Yordam`,
+  };
+  const onlineDoctors = [
+    { label: `Nevropotolog`, path: `/onlineDoctor` },
+    { label: `Genetika`, path: `/onlineDoctor` },
+    { label: `Stomatologiya`, path: `/onlineDoctor` },
+    { label: `Jarrohlik`, path: `/onlineDoctor` },
+    { label: `Hammasi +`, path: `/onlineDoctor` },
+  ];
+  const products = [
+    { label: `Choy`, path: `/products` },
+    { label: `Yog'lar`, path: `/products` },
+    { label: `Ziravorlar`, path: `/products` },
+    { label: `Asal`, path: `/products` },
+    { label: `Tabletkalar`, path: `/products` },
+    { label: `Hammasi +`, path: `/products` },
+  ];
+  const news = [
+    { label: `Yangiliklar`, path: `/news` },
+    { label: `Salomatlik sari`, path: `/news` },
+  ];
+  const helps = [
+    { label: `Yordam`, path: `/help` },
+    { label: `Ko'p so'raladigan savollar`, path: `/help` },
   ];
   const [adminRouter, setAdminRouter] = useState(false);
   const dispatch = useDispatch();
@@ -87,45 +65,72 @@ const Navigate2 = () => {
           </NavLink>
 
           <div className="navcontainer">
-            {navinfo.map((item, index) => (
-              <div key={index} className={`nav_Head `}>
-                {item.dropdown ? (
-                  <Dropdown
-                    title={item.title}
-                    link={item.link}
-                    items={item.dropdown}
-                    setLink={setLink}
-                  />
-                ) : (
-                  <NavLink
-                    className="nav_Item"
-                    to={item.link}
-                    onClick={() => changeUserRouter(item.set)}
-                  >
-                    {item.title}
-                  </NavLink>
-                )}
-              </div>
-            ))}
+            <div className="navTitles">
+              <li className="title1">
+                <Link to="/">{navElements.title1}</Link>
+              </li>
+              <li className="title2 item">
+                {navElements.title2}
+                <ul>
+                  {onlineDoctors.map((item) => (
+                    <Link to={item.path}>
+                      <li>{item.label}</li>
+                    </Link>
+                  ))}
+                </ul>
+              </li>
+              <li className="title3 item">
+                {navElements.title3}
+                <ul>
+                  {products.map((item) => (
+                    <Link to={item.path}>
+                      <li>{item.label}</li>
+                    </Link>
+                  ))}
+                </ul>
+              </li>
+              <li className="title4"><Link to="/aboutUs">{navElements.title4}</Link></li>
+              <li className="title5 item">
+                {navElements.title5}
+                <ul>
+                  {news.map((item) => (
+                    <Link to={item.path}>
+                      <li>{item.label}</li>
+                    </Link>
+                  ))}
+                </ul>
+              </li>
+              <li className="title6 item">
+                {navElements.title6}
+                <ul>
+                  {helps.map((item) => (
+                    <Link to={item.path}>
+                      <li>{item.label}</li>
+                    </Link>
+                  ))}
+                </ul>
+              </li>
+            </div>
           </div>
 
           <div className="nav-icons">
-          { user !== null ?
-           <img
-              className="nav-icons__icon"
-              src={NavIcon1}
-              alt="icon"
-              onClick={() => changeAdminRouter()}
-            /> 
-            :  <Link to={'/favoriteDoctors'}>
-            <img
-            className="nav-icons__icon"
-            src={NavIcon1}
-            alt="icon" />
-            </Link>
-            }
+            {user !== null ? (
+              <img
+                className="nav-icons__icon"
+                src={NavIcon1}
+                alt="icon"
+                onClick={() => changeAdminRouter()}
+              />
+            ) : (
+              <Link to={"/favoriteDoctors"}>
+                <img className="nav-icons__icon" src={NavIcon1} alt="icon" />
+              </Link>
+            )}
 
-         <Link to={'/favoriteDrug'}>   <img className="nav-icons__icon" src={NavIcon2} alt="icon" /></Link>
+            <Link to={"/favoriteDrug"}>
+              {" "}
+              <img className="nav-icons__icon" src={NavIcon2} alt="icon" />
+            </Link>
             <img
               className="nav-icons__icon"
               src={NavIcon2}
@@ -133,7 +138,9 @@ const Navigate2 = () => {
               hidden={link}
             />
 
-            <Link to={'/basket'}><img className="nav-icons__icon" src={NavIcon3} alt="icon" /></Link>
+            <Link to={"/basket"}>
+              <img className="nav-icons__icon" src={NavIcon3} alt="icon" />
+            </Link>
           </div>
         </div>
       </div>
