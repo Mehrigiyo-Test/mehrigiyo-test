9a4193fbc3cf6195f1c7b33b02d5732d7bd34afd
Uraimoff <uraimovibrohim5@gmail.com>
2022-06-05 12:29:13 +0500

    pullin sorted obf

--- src/components/Admin/AdminRouter/AdminRouter.js
+++ src/components/Admin/AdminRouter/AdminRouter.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes} from 'react-router-dom'
 import LayoutAdmin from '../Layout/Layout'
 import routes from "../../../routes"
+
 function AdminRouter() {
  
   return (
